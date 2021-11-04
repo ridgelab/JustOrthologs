54971ff5d8720ad0e2652a767e52d6bdc8101e46
miller34 <justin@ke7gts.com>
2021-11-04 14:35:34 -0400

    Ensure python2 is called

--- run_multiple_species.sh
+++ run_multiple_species.sh
@@ -51,9 +51,9 @@ for a; do
     shift
     for b; do
 		file_b=`basename ${b%%.*}`
-		python wrapper.py -g1 "${a}${gff_extension}" -g2 "${b}${gff_extension}" -r1 "${a}${fasta_extension}" -r2 "${b}${fasta_extension}" -all -o ${output_dir}/${file_a}_${file_b}
+		python2 wrapper.py -g1 "${a}${gff_extension}" -g2 "${b}${gff_extension}" -r1 "${a}${fasta_extension}" -r2 "${b}${fasta_extension}" -all -o ${output_dir}/${file_a}_${file_b}
     done
 done
 
-python combineOrthoGroups.py -id ${output_dir} -o ${end_output}
+python2 combineOrthoGroups.py -id ${output_dir} -o ${end_output}
 
